5318681f1f687b624861729d1897c52a0b28aeff
Arthur Khakimov <archihaki@gmail.com>
2021-08-26 03:32:51 +0000

    Update hw9

--- hw9_problem9.py
+++ hw9_problem9.py
@@ -1,2 +1,2 @@
 #!/usr/bin/env python3
-print([a*b*c for a in range(1,1001) for b in range(a,1001) for c in range(b,1001) if a+b+c==1000 and a**2+b**2==c**2][0])
+print([a*b*(1000-a-b) for a in range(1,1001) for b in range(a,1001) if a**2+b**2==(1000-a-b)**2][0])
