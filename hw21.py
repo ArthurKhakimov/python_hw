729d9b8242f33aee8dcf7e8fd3fc4f9c047bef1f
Arthur Khakimov <archihaki@gmail.com>
2021-09-07 15:09:13 +0000

    Correction hw5,hw13,hw18,hw21

--- hw21.py
+++ hw21.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
+# Понял свою ошибку, теперь по окончании генератора функция проверки будет возвращать None.
+
 def merge(a, b):
     def stop_iteration_trap(i):
         try:
             result = next(i)
         except StopIteration:
-            result = "Stop"
+            result = None
         return result
 
     iter_a = stop_iteration_trap(a)
     iter_b = stop_iteration_trap(b)
-    while iter_a != "Stop" or iter_b != "Stop":
-        if iter_a != "Stop" and iter_b != "Stop":
+    while iter_a != None or iter_b != None:
+        if iter_a != None and iter_b != None:
             if iter_a >= iter_b:
                 yield iter_b
                 iter_b = stop_iteration_trap(b)
             elif iter_a < iter_b:
                 yield iter_a
                 iter_a = stop_iteration_trap(a)
-        elif iter_a != "Stop" and iter_b == "Stop":
-            while iter_a != "Stop":
+        elif iter_a != None and iter_b == None:
+            while iter_a != None:
                 yield iter_a
                 iter_a = stop_iteration_trap(a)
-        elif iter_b != "Stop" and iter_a == "Stop":
-            while iter_b != "Stop":
+        elif iter_b != None and iter_a == None:
+            while iter_b != None:
                 yield iter_b
                 iter_b = stop_iteration_trap(b)
 
 #print(list(x for x in range(1,4)), list(x for x in range(1,10)))
-#print(list(merge((x for x in range(-1,4)),(x for x in range(1,10)))))
+#print(list(merge((x for x in range(1,4)),(x for x in range(1,10)))))
